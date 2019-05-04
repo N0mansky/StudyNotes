5aaff0816652a50ce0f90b30483d5f8cf821b239
N0mansky <fueuru@163.com>
2019-05-04 16:21:40 +0800

    Add another method to reverse one-way linklist

--- Python/codes/llist_reverse.py
+++ Python/codes/llist_reverse.py
@@ -1,40 +1,61 @@
 import unittest
+
 """
 Reversing one-way link-list
 e.g.,1->2->3->4 reversed to 2->1->4->3
 """
+
+
 class LinkNode(object):
-    def __init__(self,val,pt):
+    def __init__(self, val, pt):
         self.val = val
         self.pt = pt
 
 
+# First method,modify the val of nodes
 def pair_rev(head):
     if head is None: return
     curr = head
     while curr is not None:
         if curr.pt is not None:
-            curr.val,curr.pt.val = curr.pt.val,curr.val
+            curr.val, curr.pt.val = curr.pt.val, curr.val
             curr = curr.pt.pt
         else:
             break
 
-class TestLink(unittest.TestCase):
 
-    expected1 = [2,1,4,3,5]
-    expected2 = [2,1,4,3,6,5]
+def pair_rev2(head):
+    if head != None and head.pt != None:
+        pt = head.pt
+        head.pt = pair_rev2(pt.pt)
+        pt.pt = head
+        return pt
+    return head
+
+
+class TestLink(unittest.TestCase):
+    expected1 = [2, 1, 4, 3, 5]
+    expected2 = [2, 1, 4, 3, 6, 5]
 
     def setUp(self):
-        self.head1 = self.crt_links(1,5)
-        self.head2 = self.crt_links(1,6)
+        self.head1 = self.crt_links(1, 5)
+        self.head2 = self.crt_links(1, 6)
 
     def test_pair_rev(self):
         pair_rev(self.head1)
         pair_rev(self.head2)
-        rst1=self.prt_link(self.head1)
-        rst2=self.prt_link(self.head2)
-        self.assertEqual(rst1,self.expected1)
-        self.assertEqual(rst2,self.expected2)
+        rst1 = self.prt_link(self.head1)
+        rst2 = self.prt_link(self.head2)
+        self.assertEqual(rst1, self.expected1)
+        self.assertEqual(rst2, self.expected2)
+
+    def test_pair_rev2(self):
+        self.head1 = pair_rev2(self.head1)
+        self.head2 = pair_rev2(self.head2)
+        rst1 = self.prt_link(self.head1)
+        rst2 = self.prt_link(self.head2)
+        self.assertEqual(rst1, self.expected1)
+        self.assertEqual(rst2, self.expected2)
 
     @staticmethod
     def prt_link(head):
@@ -47,14 +68,15 @@ class TestLink(unittest.TestCase):
         return l
 
     @staticmethod
-    def crt_links(start,end):
-        head = LinkNode(start,None)
+    def crt_links(start, end):
+        head = LinkNode(start, None)
         curr = head
-        for x in range(start+1,end+1):
-            tmp = LinkNode(x,None)
+        for x in range(start + 1, end + 1):
+            tmp = LinkNode(x, None)
             curr.pt = tmp
             curr = tmp
         return head
+
 
 if __name__ == '__main__':
     unittest.main()
